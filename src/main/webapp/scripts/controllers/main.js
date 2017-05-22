ce923b7232114a3a8e17bfb9192c0c06616dc3c4
Michael Goddard <mgoddard@gopivotal.com>
2017-05-22 14:18:07 -0400

    Changes to the UI so that positive sentiment shows a 'thumbs up' icon, and non-positive shows 'thumbs down' (DOESN'T WORK YET)

--- src/main/webapp/scripts/controllers/main.js
+++ src/main/webapp/scripts/controllers/main.js
@@ -7,49 +7,55 @@
  * Controller of the sbAdminApp
  */
 angular.module('sbAdminApp')
-  .controller('MainCtrl', function($scope,$position,$http) {
+    .controller('MainCtrl', function ($scope, $position, $http) {
 
-    var loaddata = function(){
+        var loaddata = function () {
 
             // EDIT THIS TO CORRESPOND TO THE URL OF YOUR "ds_app_15"
             $http.get('http://ds_app_15.apps.pcfongcp.com/lastMessage')
-            .then(function(data){
+                .then(function (data) {
 
-                console.log(data);
-                $scope.sentimentdata = data.data;
+                    console.log(data);
+                    $scope.sentimentdata = data.data;
 
-                // check for vision_labels
+                    // check for vision_labels
 
-                var media = $scope.sentimentdata.entities.media;
+                    var media = $scope.sentimentdata.entities.media;
 
-                $scope.media_url = media[0].media_url;
+                    $scope.media_url = media[0].media_url;
 
-                $scope.vision_labels = media[0].vision_labels;
-                console.log($scope.vision_labels);
+                    $scope.vision_labels = media[0].vision_labels;
+                    console.log($scope.vision_labels);
 
-                $scope.sentiment = $scope.sentimentdata.sentiment;
+                    $scope.sentiment.score = 0.0;
 
-                console.log($scope.sentiment);
+                    $scope.sentiment = $scope.sentimentdata.sentiment;
 
-                $scope.offer_items = $scope.sentimentdata.offer_items;
+                    // Add an icon to represent the sentiment
+                    if ($scope.sentiment.score > 0.0) {
+                        $scope.sentiment_icon_url = "/Thumbs_Up.png";
+                    } else {
+                        $scope.sentiment_icon_url = "/Thumbs_Down.png";
+                    }
 
-                $scope.offer_text = $scope.sentimentdata.offer_text;
+                    console.log($scope.sentiment);
 
-                console.log($scope.offer_items);
+                    $scope.offer_items = $scope.sentimentdata.offer_items;
+
+                    $scope.offer_text = $scope.sentimentdata.offer_text;
+
+                    console.log($scope.offer_items);
 
 
-            },function(errorPayload) {
+                }, function (errorPayload) {
 
                     console.log(errorPayload);
 
 
-           });
-    }
+                });
+        }
 
-    loaddata();
-
-
+        loaddata();
 
 
-
-  });
+    });
